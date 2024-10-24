a8cfa1cbddbbe1afcb9ed18f407724eec8a43b99
Prajjawal Kansara <prajjawalkansara@gmail.com>
2024-10-24 17:32:25 +0530

    updated index.txt

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
+import path from "path"
+import react from "@vitejs/plugin-react"
+// import eslint from 'vite-plugin-eslint';
+import { defineConfig } from "vite"
+ 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),  // Configure the @ alias to point to your src folder
+    },
+  },
 })
