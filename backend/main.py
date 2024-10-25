e2feff987361c8ac6472131a655c1f5ba225e2fb
Prajjawal Kansara <prajjawalkansara@gmail.com>
2024-10-25 12:13:06 +0530

    responsive backend

--- backend/main.py
+++ backend/main.py
@@ -3,6 +3,7 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
 from constants import SERVER_URL, PORT, ENV
+from apps.caclulator.route import router as calculator_router
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -21,6 +22,8 @@ app.add_middleware(
 @app.get('/')
 async def root():
     return {"message": "Server is running"}
+
+app.include_router(calculator_router, prefix="/calculate", tags=["calculate"])
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host=SERVER_URL, port=int(PORT), reload=(ENV == "dev"))
